b04abbfb05dde4580025f57e7e18c09f4c472ac8
sage <laymonage@gmail.com>
2017-11-03 06:38:10 +0700

    Test kitchensink app

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 '''
 AidenBot
-Early test: echobot
+Early test 2: kitchensink
 '''
 
+from __future__ import unicode_literals
+
+import errno
 import os
 import sys
+import tempfile
 from argparse import ArgumentParser
 
 from flask import Flask, request, abort
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -16,6 +21,14 @@ from linebot.exceptions import (
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
+    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn,
+    URITemplateAction, PostbackTemplateAction, DatetimePickerTemplateAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage, FileMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
 )
 
 app = Flask(__name__)
@@ -33,21 +46,35 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+
+
+def make_static_tmp_dir():
+    '''
+    Create temporary directory for download content
+    '''
+    try:
+        os.makedirs(static_tmp_path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(static_tmp_path):
+            pass
+        else:
+            raise
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
     '''
-    Webhook function
+    Webhook callback function
     '''
-
-    # Get X-Line-Signature header value
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
-    # Get request body as text
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # Handle webhook body
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
@@ -57,14 +84,268 @@ def callback():
 
 
 @handler.add(MessageEvent, message=TextMessage)
-def message_text(event):
+def handle_text_message(event):
     '''
-    Echo received messages
+    Text message handler
+    '''
+    text = event.message.text
+
+    if text == 'profile':
+        if isinstance(event.source, SourceUser):
+            profile = line_bot_api.get_profile(event.source.user_id)
+            line_bot_api.reply_message(
+                event.reply_token, [
+                    TextSendMessage(
+                        text='Display name: ' + profile.display_name
+                    ),
+                    TextSendMessage(
+                        text='Status message: ' + profile.status_message
+                    )
+                ]
+            )
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextMessage(text="Bot can't use profile API without user ID"))
+
+    elif text == 'bye':
+        if isinstance(event.source, SourceGroup):
+            line_bot_api.reply_message(
+                event.reply_token, TextMessage(text='Leaving group'))
+            line_bot_api.leave_group(event.source.group_id)
+        elif isinstance(event.source, SourceRoom):
+            line_bot_api.reply_message(
+                event.reply_token, TextMessage(text='Leaving group'))
+            line_bot_api.leave_room(event.source.room_id)
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextMessage(text="Bot can't leave from 1:1 chat"))
+
+    elif text == 'confirm':
+        confirm_template = ConfirmTemplate(text='Do it?', actions=[
+            MessageTemplateAction(label='Yes', text='Yes!'),
+            MessageTemplateAction(label='No', text='No!'),
+        ])
+        template_message = TemplateSendMessage(
+            alt_text='Confirm alt text', template=confirm_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+
+    elif text == 'buttons':
+        buttons_template = ButtonsTemplate(
+            title='My buttons sample', text='Hello, my buttons', actions=[
+                URITemplateAction(
+                    label='Go to line.me', uri='https://line.me'),
+                PostbackTemplateAction(label='ping', data='ping'),
+                PostbackTemplateAction(
+                    label='ping with text', data='ping',
+                    text='ping'),
+                MessageTemplateAction(label='Translate Rice', text='米')
+            ])
+        template_message = TemplateSendMessage(
+            alt_text='Buttons alt text', template=buttons_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+
+    elif text == 'carousel':
+        carousel_template = CarouselTemplate(columns=[
+            CarouselColumn(text='hoge1', title='fuga1', actions=[
+                URITemplateAction(
+                    label='Go to line.me', uri='https://line.me'),
+                PostbackTemplateAction(label='ping', data='ping')
+            ]),
+            CarouselColumn(text='hoge2', title='fuga2', actions=[
+                PostbackTemplateAction(
+                    label='ping with text', data='ping',
+                    text='ping'),
+                MessageTemplateAction(label='Translate Rice', text='米')
+            ]),
+        ])
+        template_message = TemplateSendMessage(
+            alt_text='Carousel alt text', template=carousel_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+
+    elif text == 'image_carousel':
+        image_carousel_template = ImageCarouselTemplate(columns=[
+            ImageCarouselColumn(
+                image_url='https://via.placeholder.com/1024x1024',
+                action=DatetimePickerTemplateAction(label='datetime',
+                                                    data='datetime_postback',
+                                                    mode='datetime')),
+            ImageCarouselColumn(
+                image_url='https://via.placeholder.com/1024x1024',
+                action=DatetimePickerTemplateAction(label='date',
+                                                    data='date_postback',
+                                                    mode='date'))
+        ])
+        template_message = TemplateSendMessage(
+            alt_text='ImageCarousel alt text',
+            template=image_carousel_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+
+    elif text == 'imagemap':
+        pass
+
+    else:
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=event.message.text))
+
+
+@handler.add(MessageEvent, message=LocationMessage)
+def handle_location_message(event):
+    '''
+    Location message handler
     '''
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+        LocationSendMessage(
+            title=event.message.title, address=event.message.address,
+            latitude=event.message.latitude, longitude=event.message.longitude
+        )
     )
+
+
+@handler.add(MessageEvent, message=StickerMessage)
+def handle_sticker_message(event):
+    '''
+    Sticker message handler
+    '''
+    line_bot_api.reply_message(
+        event.reply_token,
+        StickerSendMessage(
+            package_id=event.message.package_id,
+            sticker_id=event.message.sticker_id)
+    )
+
+
+@handler.add(MessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
+def handle_content_message(event):
+    '''
+    Other message types handler
+    '''
+    if isinstance(event.message, ImageMessage):
+        ext = 'jpg'
+    elif isinstance(event.message, VideoMessage):
+        ext = 'mp4'
+    elif isinstance(event.message, AudioMessage):
+        ext = 'm4a'
+    else:
+        return
+
+    message_content = line_bot_api.get_message_content(event.message.id)
+    with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-',
+                                     delete=False) as tf:
+        for chunk in message_content.iter_content():
+            tf.write(chunk)
+        tempfile_path = tf.name
+
+    dist_path = tempfile_path + '.' + ext
+    dist_name = os.path.basename(dist_path)
+    os.rename(tempfile_path, dist_path)
+
+    line_bot_api.reply_message(
+        event.reply_token, [
+            TextSendMessage(text='Save content.'),
+            TextSendMessage(text=request.host_url +
+                            os.path.join('static', 'tmp', dist_name))
+        ])
+
+
+@handler.add(MessageEvent, message=FileMessage)
+def handle_file_message(event):
+    '''
+    File message handler
+    '''
+    message_content = line_bot_api.get_message_content(event.message.id)
+    with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix='file-',
+                                     delete=False) as tf:
+        for chunk in message_content.iter_content():
+            tf.write(chunk)
+        tempfile_path = tf.name
+
+    dist_path = tempfile_path + '-' + event.message.file_name
+    dist_name = os.path.basename(dist_path)
+    os.rename(tempfile_path, dist_path)
+
+    line_bot_api.reply_message(
+        event.reply_token, [
+            TextSendMessage(text='Save file.'),
+            TextSendMessage(text=request.host_url +
+                            os.path.join('static', 'tmp', dist_name))
+        ])
+
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+    '''
+    Follow event handler
+    '''
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='Got follow event')
+        )
+
+
+@handler.add(UnfollowEvent)
+def handle_unfollow():
+    '''
+    Unfollow event handler
+    '''
+    app.logger.info("Got Unfollow event")
+
+
+@handler.add(JoinEvent)
+def handle_join(event):
+    '''
+    Join event handler
+    '''
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='Joined this ' + event.source.type)
+        )
+
+
+@handler.add(LeaveEvent)
+def handle_leave():
+    '''
+    Leave event handler
+    '''
+    app.logger.info("Got leave event")
+
+
+@handler.add(PostbackEvent)
+def handle_postback(event):
+    '''
+    Postback event handler
+    '''
+    if event.postback.data == 'ping':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='pong')
+            )
+    elif event.postback.data == 'datetime_postback':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.postback.params['datetime'])
+            )
+    elif event.postback.data == 'date_postback':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.postback.params['date'])
+            )
+
+
+@handler.add(BeaconEvent)
+def handle_beacon(event):
+    '''
+    Beacon event handler
+    '''
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(
+            text=('Got beacon event. hwid={}, device_message(hex string)={}'
+                  .format(event.beacon.hwid, event.beacon.dm))
+            )
+        )
 
 
 if __name__ == "__main__":
@@ -74,6 +355,9 @@ if __name__ == "__main__":
     arg_parser.add_argument('-p', '--port', default=8000, help='port')
     arg_parser.add_argument('-d', '--debug', default=False, help='debug')
     options = arg_parser.parse_args()
+
+    # Create temporary directory for download content
+    make_static_tmp_dir()
     port = int(os.environ.get('PORT', 5000))
 
     app.run(host='0.0.0.0', debug=options.debug, port=port)

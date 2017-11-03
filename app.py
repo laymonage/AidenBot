7dc227c05e279d8fe62ae271cbd22f4bf7c8e6ae
sage <laymonage@gmail.com>
2017-11-03 19:17:00 +0700

    Use essential handlers only
    Other handlers aren't really needed right now

--- app.py
+++ app.py
@@ -21,14 +21,8 @@ from linebot.exceptions import (
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
-    SourceUser, SourceGroup, SourceRoom,
-    TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
-    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn,
-    URITemplateAction, PostbackTemplateAction, DatetimePickerTemplateAction,
-    CarouselTemplate, CarouselColumn, PostbackEvent,
-    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
-    ImageMessage, VideoMessage, AudioMessage, FileMessage,
-    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
+    SourceUser, SourceGroup, SourceRoom, FileMessage,
+    UnfollowEvent, LeaveEvent
 )
 
 import wikipedia
@@ -125,69 +119,6 @@ def handle_text_message(event):
                 event.reply_token,
                 TextMessage(text="Bot can't leave from 1:1 chat"))
 
-    elif text == 'confirm':
-        confirm_template = ConfirmTemplate(text='Do it?', actions=[
-            MessageTemplateAction(label='Yes', text='Yes!'),
-            MessageTemplateAction(label='No', text='No!'),
-        ])
-        template_message = TemplateSendMessage(
-            alt_text='Confirm alt text', template=confirm_template)
-        line_bot_api.reply_message(event.reply_token, template_message)
-
-    elif text == 'buttons':
-        buttons_template = ButtonsTemplate(
-            title='My buttons sample', text='Hello, my buttons', actions=[
-                URITemplateAction(
-                    label='Go to line.me', uri='https://line.me'),
-                PostbackTemplateAction(label='ping', data='ping'),
-                PostbackTemplateAction(
-                    label='ping with text', data='ping',
-                    text='ping'),
-                MessageTemplateAction(label='Translate Rice', text='米')
-            ])
-        template_message = TemplateSendMessage(
-            alt_text='Buttons alt text', template=buttons_template)
-        line_bot_api.reply_message(event.reply_token, template_message)
-
-    elif text == 'carousel':
-        carousel_template = CarouselTemplate(columns=[
-            CarouselColumn(text='hoge1', title='fuga1', actions=[
-                URITemplateAction(
-                    label='Go to line.me', uri='https://line.me'),
-                PostbackTemplateAction(label='ping', data='ping')
-            ]),
-            CarouselColumn(text='hoge2', title='fuga2', actions=[
-                PostbackTemplateAction(
-                    label='ping with text', data='ping',
-                    text='ping'),
-                MessageTemplateAction(label='Translate Rice', text='米')
-            ]),
-        ])
-        template_message = TemplateSendMessage(
-            alt_text='Carousel alt text', template=carousel_template)
-        line_bot_api.reply_message(event.reply_token, template_message)
-
-    elif text == 'image_carousel':
-        image_carousel_template = ImageCarouselTemplate(columns=[
-            ImageCarouselColumn(
-                image_url='https://via.placeholder.com/1024x1024',
-                action=DatetimePickerTemplateAction(label='datetime',
-                                                    data='datetime_postback',
-                                                    mode='datetime')),
-            ImageCarouselColumn(
-                image_url='https://via.placeholder.com/1024x1024',
-                action=DatetimePickerTemplateAction(label='date',
-                                                    data='date_postback',
-                                                    mode='date'))
-        ])
-        template_message = TemplateSendMessage(
-            alt_text='ImageCarousel alt text',
-            template=image_carousel_template)
-        line_bot_api.reply_message(event.reply_token, template_message)
-
-    elif text == 'imagemap':
-        pass
-
     elif text[0] == '/':
         command = text[1:]
         if command.lower().startswith('wiki '):
@@ -232,66 +163,6 @@ def handle_text_message(event):
                 )
 
 
-@handler.add(MessageEvent, message=LocationMessage)
-def handle_location_message(event):
-    '''
-    Location message handler
-    '''
-    line_bot_api.reply_message(
-        event.reply_token,
-        LocationSendMessage(
-            title=event.message.title, address=event.message.address,
-            latitude=event.message.latitude, longitude=event.message.longitude
-        )
-    )
-
-
-@handler.add(MessageEvent, message=StickerMessage)
-def handle_sticker_message(event):
-    '''
-    Sticker message handler
-    '''
-    line_bot_api.reply_message(
-        event.reply_token,
-        StickerSendMessage(
-            package_id=event.message.package_id,
-            sticker_id=event.message.sticker_id)
-    )
-
-
-@handler.add(MessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
-def handle_content_message(event):
-    '''
-    Other message types handler
-    '''
-    if isinstance(event.message, ImageMessage):
-        ext = 'jpg'
-    elif isinstance(event.message, VideoMessage):
-        ext = 'mp4'
-    elif isinstance(event.message, AudioMessage):
-        ext = 'm4a'
-    else:
-        return
-
-    message_content = line_bot_api.get_message_content(event.message.id)
-    with tempfile.NamedTemporaryFile(dir=static_tmp_path, prefix=ext + '-',
-                                     delete=False) as tf:
-        for chunk in message_content.iter_content():
-            tf.write(chunk)
-        tempfile_path = tf.name
-
-    dist_path = tempfile_path + '.' + ext
-    dist_name = os.path.basename(dist_path)
-    os.rename(tempfile_path, dist_path)
-
-    line_bot_api.reply_message(
-        event.reply_token, [
-            TextSendMessage(text='Save content.'),
-            TextSendMessage(text=request.host_url +
-                            os.path.join('static', 'tmp', dist_name))
-        ])
-
-
 @handler.add(MessageEvent, message=FileMessage)
 def handle_file_message(event):
     '''
@@ -310,21 +181,9 @@ def handle_file_message(event):
 
     line_bot_api.reply_message(
         event.reply_token, [
-            TextSendMessage(text='Save file.'),
-            TextSendMessage(text=request.host_url +
+            TextSendMessage(text="Mirror: " + request.host_url +
                             os.path.join('static', 'tmp', dist_name))
         ])
-
-
-@handler.add(FollowEvent)
-def handle_follow(event):
-    '''
-    Follow event handler
-    '''
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='Got follow event')
-        )
 
 
 @handler.add(UnfollowEvent)
@@ -335,59 +194,12 @@ def handle_unfollow():
     app.logger.info("Got Unfollow event")
 
 
-@handler.add(JoinEvent)
-def handle_join(event):
-    '''
-    Join event handler
-    '''
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='Joined this ' + event.source.type)
-        )
-
-
 @handler.add(LeaveEvent)
 def handle_leave():
     '''
     Leave event handler
     '''
     app.logger.info("Got leave event")
-
-
-@handler.add(PostbackEvent)
-def handle_postback(event):
-    '''
-    Postback event handler
-    '''
-    if event.postback.data == 'ping':
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text='pong')
-            )
-    elif event.postback.data == 'datetime_postback':
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.postback.params['datetime'])
-            )
-    elif event.postback.data == 'date_postback':
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.postback.params['date'])
-            )
-
-
-@handler.add(BeaconEvent)
-def handle_beacon(event):
-    '''
-    Beacon event handler
-    '''
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(
-            text=('Got beacon event. hwid={}, device_message(hex string)={}'
-                  .format(event.beacon.hwid, event.beacon.dm))
-            )
-        )
 
 
 if __name__ == "__main__":

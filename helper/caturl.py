19fd3ef6418970bc961003dfe471856020c8cce1
sage <laymonage@gmail.com>
2018-08-05 11:27:41 +0700

    Revert "Mirror /cat images to Dropbox"
    This reverts commit 445bb95b13348f543fb0c04ffd2b8899e6cb38be.

--- helper/caturl.py
+++ helper/caturl.py
@@ -3,11 +3,7 @@ TheCatAPI helper module
 (c) 2018 - laymonage
 '''
 
-import os
 import requests
-from .dropson import dbx_ul, dbx_tl
-
-CAT_PATH = os.getenv('CAT_PATH', None)
 
 
 def cat():
@@ -17,6 +13,4 @@ def cat():
     url = 'http://thecatapi.com/api/images/get'
     req = requests.get(url)
     url = req.url.replace('http://', 'https://')
-    path = CAT_PATH + url[url.find('/tumblr_'):]
-    dbx_ul(req.content, path)
-    return dbx_tl(path)
+    return url
--- helper/trap.py
+++ helper/trap.py
@@ -5,7 +5,7 @@
 
 import os
 import random
-from .caturl import cat
+import requests
 from .dropson import dbx_dl, get_json
 
 
@@ -13,10 +13,13 @@ def surprise(safe=False):
     '''
     ?
     '''
-    prev_url = cat()
+    cat_api = 'http://thecatapi.com/api/images/get'
+    prev_url = requests.get(cat_api)
+    prev_url = prev_url.url.replace('http://', 'https://')
 
     if safe:
-        orig_url = cat()
+        orig_url = requests.get(cat_api)
+        orig_url = orig_url.url.replace('http://', 'https://')
     else:
         surprise_links = os.getenv('SURPRISES_FILE_PATH', None)
         surprises = get_json(dbx_dl(surprise_links))

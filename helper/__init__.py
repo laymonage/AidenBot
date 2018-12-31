a201087db3302678a06719f7a8def030683539a8
shivaroast <46077976+shivaroast@users.noreply.github.com>
2018-12-31 14:54:57 -0800

    Update __init__.py

--- helper/__init__.py
+++ helper/__init__.py
@@ -29,5 +29,5 @@ from .wiki import wiki_get, wiki_lang
 from .wolframalpha import wolfram
 from .wunderground import weather
 from ._wrapper import (
-    cat_wrap, curx_wrap, stalkig_wrap, wolfram_wrap
+    cat_wrap, curx_wrap, stalkig_wrap, surprise_wrap, wolfram_wrap
 )

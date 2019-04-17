ea4d65d8440a62b87d6bc1861908f818f5411ee9
sage <laymonage@gmail.com>
2019-04-17 13:46:13 +0700

    Update kbbi to 0.3.1

--- helper/kbbih.py
+++ helper/kbbih.py
@@ -9,7 +9,7 @@ from kbbi import KBBI
 
 def kbbi_def(keyword, ex=False):
     """
-    Return an entry of keyword in KBBI.
+    Return scraped page of keyword in KBBI.
 
     keyword (str): keyword to look up in KBBI
     ex (bool): return with examples (if any)
@@ -19,9 +19,5 @@ def kbbi_def(keyword, ex=False):
     except KBBI.TidakDitemukan as error:
         result = str(error)
     else:
-        result = "Definisi {}:\n".format(keyword)
-        if ex:
-            result += '\n'.join(entry.arti_contoh)
-        else:
-            result += str(entry)
+        result = entry.__str__(contoh=ex)
     return result
